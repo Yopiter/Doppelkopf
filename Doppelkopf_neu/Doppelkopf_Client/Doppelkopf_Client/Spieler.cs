4cbed8df8894042229e51fcf56f001ae2c7225a8
Martin.Schneider <Martin.Schneider@quodata.de>
2017-08-10 17:16:01 +0200

    Übertragung von Ansagen

--- Doppelkopf_neu/Doppelkopf_Client/Doppelkopf_Client/Spieler.cs
+++ Doppelkopf_neu/Doppelkopf_Client/Doppelkopf_Client/Spieler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,8 +17,7 @@ namespace Doppelkopf_Client
         public Button btKarte;
         public TextBox tbLabel;
         public int AbsolutePositionAufServer;
-
-        public enum Zustand { Re, HochzeitAlte, HochzeitOhneAlte, Solo, Kontra };
+        List<PictureBox> Zustandsboxen = new List<PictureBox>();
 
         public Spieler(string name, Button button, TextBox labelbox, int Position)
         {
@@ -28,9 +28,23 @@ namespace Doppelkopf_Client
             tbLabel = labelbox;
         }
 
-        public void AddZustand(Zustand NeuerZustand)
+        public void AddZustand(Zustand NeuerZustand, PictureBox newBox)
         {
+            switch (NeuerZustand)
+            {
+                case (Zustand.Re):
+                    newBox.Image = new Bitmap("Ress\\Icons\\Re.png");
+                    break;
+                case (Zustand.Kontra):
+                    newBox.Image = new Bitmap("Ress\\Icons\\Re.png");
+                    break;
+                default:
+                    MessageBox.Show("Oh no, Your Player " + Name + " seems to be retarted. He thinks he can be " + NeuerZustand.ToString());
+                    break;
+            }
+            Zustandsboxen.Add(newBox);
             ZustandsList.Add(NeuerZustand);
+
         }
     }
 }
